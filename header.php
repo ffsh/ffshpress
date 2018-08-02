4d52d77e149c4366a6a42f4d18af5d29177d827f
Benjamin Brahmer <info@b-brahmer.de>
2018-08-02 11:43:55 +0200

    switch to container-fluid

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <body <?php body_class(); ?>>
 
 <div class="blog-masthead">
-    <div class="container">
+    <div class="container-fluid">
         <nav class="blog-nav">
             <a class="blog-nav-item active" href="#">Home</a>
             <a class="blog-nav-item" href="#">New features</a>
