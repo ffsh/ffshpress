7fe55cb7ff37b44020f8bf8b1f5fe6ccf6e986df
Benjamin Brahmer <info@b-brahmer.de>
2018-09-27 16:50:22 +0200

    rename all the things

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div><!-- /.container -->
 
 <footer class="blog-footer">
-    <p>Powerd by <a href="http://wordpress.com.com">Wordpress</a>. Theme by <a href="https://github.com/Grotax/grotaxblog">Grotax</a>.</p>
+    <p>Powerd by <a href="http://wordpress.com.com">Wordpress</a>. Theme by <a href="https://github.com/ffsh/ffshpress">ffsh</a>.</p>
     <p><a href="#">Back to top</a></p>
 </footer>
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,29 +1,29 @@
 <?php
 
-function grotaxblog_enqueue_styles() {
+function ffshpress_enqueue_styles() {
     wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
     $dependencies = array('bootstrap');
-    wp_enqueue_style( 'grotaxblog-style', get_stylesheet_uri(), $dependencies );
+    wp_enqueue_style( 'ffshpress-style', get_stylesheet_uri(), $dependencies );
 }
 
-function grotaxblog_enqueue_scripts() {
+function ffshpress_enqueue_scripts() {
     $dependencies = array('jquery');
     wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '4.1.3', true );
 }
 
-function grotaxblog_wp_setup() {
+function ffshpress_wp_setup() {
     add_theme_support( 'title-tag' );
 }
 
-function grotaxblog_register_menu() {
+function ffshpress_register_menu() {
     register_nav_menu('header-menu', __( 'Header Menu' ));
 }
 
 
-add_action( 'wp_enqueue_scripts', 'grotaxblog_enqueue_styles' );
-add_action( 'wp_enqueue_scripts', 'grotaxblog_enqueue_scripts' );
-add_action( 'after_setup_theme', 'grotaxblog_wp_setup' );
-add_action( 'init', 'grotaxblog_register_menu' );
+add_action( 'wp_enqueue_scripts', 'ffshpress_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'ffshpress_enqueue_scripts' );
+add_action( 'after_setup_theme', 'ffshpress_wp_setup' );
+add_action( 'init', 'ffshpress_register_menu' );
 
 require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 require_once('wp_bootstrap_pagination.php');
